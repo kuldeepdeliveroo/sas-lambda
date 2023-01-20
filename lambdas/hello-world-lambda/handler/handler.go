862c6a58bb384dc26ef1b8ea3060002afabc377a
Kuldeep Yadav <kuldeep.yadav@deliveroo.com>
2023-01-20 17:45:59 +0530

    Working code

--- lambdas/hello-world-lambda/handler/handler.go
+++ lambdas/hello-world-lambda/handler/handler.go
@@ -1,13 +1,15 @@
 package handler
 
 import (
-	"context"
 	"github.com/aws/aws-lambda-go/events"
 	"log"
 )
 
-func HandleRequest(ctx context.Context, request events.ALBTargetGroupRequest) error {
+func HandleRequest() (events.APIGatewayProxyResponse, error) {
 	// This lambda will be invoked by a URL with the format similar to https://roo-domain.io/2343cf3d2
 	log.Printf("Hello World!!")
-	return nil
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "Hey hey santa",
+	}, nil
 }
